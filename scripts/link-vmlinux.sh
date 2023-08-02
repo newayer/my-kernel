0a97154d17cafc6f3b94243003050e9629b3f266
newayer <cn_gwq@sina.com>
2023-08-02 21:13:19 +0800

    同步友善scripts目录脚本修改

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -129,9 +129,6 @@ objtool_link()
 			objtoolcmd="check"
 		fi
 		objtoolopt="${objtoolopt} --vmlinux"
-		if [ -n "${CONFIG_CPU_UNRET_ENTRY}" ]; then
-			objtoolopt="${objtoolopt} --unret"
-		fi
 		if [ -z "${CONFIG_FRAME_POINTER}" ]; then
 			objtoolopt="${objtoolopt} --no-fp"
 		fi
@@ -143,9 +140,6 @@ objtool_link()
 		fi
 		if [ -n "${CONFIG_X86_SMAP}" ]; then
 			objtoolopt="${objtoolopt} --uaccess"
-		fi
-		if [ -n "${CONFIG_SLS}" ]; then
-			objtoolopt="${objtoolopt} --sls"
 		fi
 		info OBJTOOL ${1}
 		tools/objtool/objtool ${objtoolcmd} ${objtoolopt} ${1}
@@ -234,7 +228,7 @@ gen_btf()
 	vmlinux_link ${1}
 
 	info "BTF" ${2}
-	LLVM_OBJCOPY="${OBJCOPY}" ${PAHOLE} -J ${PAHOLE_FLAGS} ${1}
+	LLVM_OBJCOPY=${OBJCOPY} ${PAHOLE} -J ${1}
 
 	# Create ${2} which contains just .BTF section but no symbols. Add
 	# SHF_ALLOC because .BTF will be part of the vmlinux image. --strip-all
