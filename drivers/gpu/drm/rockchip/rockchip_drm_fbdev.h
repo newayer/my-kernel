6db87d062e9f1ef4e1641441490a46e0597b53e9
jensen <jensenhuang@friendlyarm.com>
2024-08-07 17:13:22 +0800

    drm/rockchip: add config option for drm fbdev
    It causes app failed (EBUSY) to mmap framebuffer, so add an option
    to disable it by default.
    
    Signed-off-by: jensen <jensenhuang@friendlyarm.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_fbdev.h
+++ drivers/gpu/drm/rockchip/rockchip_drm_fbdev.h
@@ -7,12 +7,14 @@
 #ifndef _ROCKCHIP_DRM_FBDEV_H
 #define _ROCKCHIP_DRM_FBDEV_H
 
-#if defined(CONFIG_DRM_FBDEV_EMULATION)
+#if defined(CONFIG_ROCKCHIP_DRM_FBDEV)
 int rockchip_drm_fbdev_init(struct drm_device *dev);
 void rockchip_drm_fbdev_fini(struct drm_device *dev);
 #else
 static inline int rockchip_drm_fbdev_init(struct drm_device *dev)
 {
+	drm_fbdev_generic_setup(dev, 0);
+
 	return 0;
 }
 
